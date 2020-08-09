64f20650ac452be012658368b8cab73a8b54b680
Molnár Balázs <molnarbalazs@pop-os.localdomain>
2020-08-09 17:46:27 +0200

    Added mongodb to rest api server

--- rest_api/index.mjs
+++ rest_api/index.mjs
@@ -8,6 +8,10 @@ import http from "http";
 import https from "https";
 import fs from "fs";
 
+const URL = "mongodb://localhost:27017";
+const OPTIONS = { useUnifiedTopology: true };
+const DBNAME = "pici";
+
 const HTTP_PORT = 1111;
 const HTTPS_PORT = 1112;
 const SSL_KEY = fs.readFileSync("credentials/ssl.key");
@@ -19,14 +23,34 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+const mongo_client = new mongodb.MongoClient(URL, OPTIONS);
+
+const httpServer = http.createServer(app);
+const httpsServer = https.createServer(CREDENTIALS, app);
+
 (async () => {
-  // pass
+  try {
+    await mongo_client.connect();
+    const db = client.db(DBNAME);
 
-  http.createServer(app).listen(HTTP_PORT, () => {
-    console.log(`HTTP server listening on port ${HTTP_PORT}`);
-  });
+    const termekCollection = db.collection("termek");
 
-  https.createServer(CREDENTIALS, app).listen(HTTPS_PORT, () => {
-    console.log(`HTTPS server listening on port ${HTTPS_PORT}`);
-  });
+    app.get("/", (req, res) => {
+      res.json({
+        name: "Pici System REST API server",
+        url: req.url,
+        ok: true,
+      });
+    });
+
+    httpServer.listen(HTTP_PORT, () => {
+      console.log(`HTTP server listening on port ${HTTP_PORT}`);
+    });
+
+    httpsServer.listen(HTTPS_PORT, () => {
+      console.log(`HTTPS server listening on port ${HTTPS_PORT}`);
+    });
+  } catch (error) {
+    console.error(error);
+  }
 })();
